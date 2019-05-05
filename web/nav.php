6905147c44e966e7181d8e362df999c0203c240e
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-04 20:33:53 -0600

    styling

--- web/nav.php
+++ web/nav.php
@@ -4,7 +4,7 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 
 <nav class="nav">
     <ul class="nav-list">
-        <li class="nav-list <?php if ($file === 'index') echo 'active' ?>"><a href="index.php" title="Home">Home</a></li>
-        <li class="nav-list <?php if ($file === 'assignment') echo 'active' ?>"><a href="assignment.php" title="Assignments">View Assignments</a></li>
+        <li class="nav-item <?php if ($file === 'index') echo 'active' ?>"><a href="index.php" title="Home">Home</a></li>
+        <li class="nav-item <?php if ($file === 'assignment') echo 'active' ?>"><a href="assignment.php" title="Assignments">View Assignments</a></li>
     </ul>
 </nav>
