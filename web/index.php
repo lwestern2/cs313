3a2bc5b606a430d9f552db4947fee7a94d5ecacb
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-04 20:39:11 -0600

    styling

--- web/index.php
+++ web/index.php
@@ -10,15 +10,14 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
     integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
-<nav>
-  <?php
-include("nav.php");
-?>
-</nav>
-
+  
 <div class="header">
     <h2 class="greeting">- Leah Western -</h2>
 </div>
+
+<?php
+include("nav.php");
+?>
 
 <body>
   <div>
