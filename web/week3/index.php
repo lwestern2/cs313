343e2406813a331e2830e5a8cfd730d32ecd2c54
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-07 17:32:18 -0600

    week 3

--- web/week3/index.php
+++ web/week3/index.php
@@ -2,46 +2,48 @@
 <html lang="en-us">
 
 <head>
-  <meta charset="UTF-8">
-  <title id="page-title">HTML Form</title>
-  <meta name="description" content="Leah Western Home page">
-  <meta name="viewport" content="width=device-width">
-  <link rel="stylesheet" type="text/css" href="style.css" media="screen">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <title id="page-title">HTML Form</title>
+    <meta name="description" content="Leah Western Home page">
+    <meta name="viewport" content="width=device-width">
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 </head>
 
 <body>
-<h1>Practice Form</h1>
-<form action="form.php" method="post"> 
- <label for="name">Name:</label>
- <input type="text" name="name"> <br>
- <label for="email">Email:</label>
- <input type="text" name="email"> <br>
+    <h1>Practice Form</h1>
+    <form action="form.php" method="post">
+        <label for="name">Name:</label>
+        <input type="text" name="name"> <br>
+        <label for="email">Email:</label>
+        <input type="text" name="email"> <br><br>
 
- <label for="major">Select major</label><br>
- <?php 
- $majorArray = array("CS"=>"Computer Science", "WDD"=>"Web Design & Development","CIT"=>"Computer Information Technology","CE"=>"Computer Engineering");
+        <label for="major">Select major</label><br>
+        <?php 
+            $majorArray = array("CS"=>"Computer Science", "WDD"=>"Web Design & Development","CIT"=>"Computer Information Technology","CE"=>"Computer Engineering");
 
- foreach($majorArray as $m =>$m_value) {
-    echo '<input type="radio" name="major" value="' . $m .'"> '. $m_value . '<br>';
- }
+            foreach($majorArray as $m =>$m_value) {
+                echo '<input type="radio" name="major" value="' . $m .'"> '. $m_value . '<br>';
+            }
  
- ?>
- <br>
+        ?>
+        <br>
 
-<label for="continent">Which continents have you been to?</label>
-<input type="checkbox" name="continent[]" value="NA"> North America <br>
-<input type="checkbox" name="continent[]" value="SA"> South America <br>
-<input type="checkbox" name="continent[]" value="EU"> Europe <br>
-<input type="checkbox" name="continent[]" value="AS"> Asia <br>
-<input type="checkbox" name="continent[]" value="AU"> Australia <br>
-<input type="checkbox" name="continent[]" value="AF"> Africa <br>
-<input type="checkbox" name="continent[]" value="AN"> Antarctica <br>
+        <label for="continent">Which continents have you been to?</label><br>
+        <input type="checkbox" name="continent[]" value="NA"> North America <br>
+        <input type="checkbox" name="continent[]" value="SA"> South America <br>
+        <input type="checkbox" name="continent[]" value="EU"> Europe <br>
+        <input type="checkbox" name="continent[]" value="AS"> Asia <br>
+        <input type="checkbox" name="continent[]" value="AU"> Australia <br>
+        <input type="checkbox" name="continent[]" value="AF"> Africa <br>
+        <input type="checkbox" name="continent[]" value="AN"> Antarctica <br> <br>
 
-<label for="name">Comments:</label><br>
- <textarea name="comments" id="comments" cols="30" rows="5"></textarea><br>
+        <label for="name">Comments:</label><br>
+        <textarea name="comments" id="comments" cols="30" rows="5"></textarea><br>
 
-<input type="submit"> 
-</form>
+        <input type="submit">
+    </form>
 </body>
+
+</html>
