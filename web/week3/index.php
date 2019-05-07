091c165dc9b4da51508e1c88f7490297704c9c4d
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-07 15:47:26 -0600

    week 3

---
+++ web/week3/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en-us">
+
+<head>
+  <meta charset="UTF-8">
+  <title id="page-title">HTML Form</title>
+  <meta name="description" content="Leah Western Home page">
+  <meta name="viewport" content="width=device-width">
+  <link rel="stylesheet" type="text/css" href="styles.css" media="screen">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+
+<body>
+<form action="form.php" method="post"> 
+ <label for="name">Name:</label>
+ <input type="text" name="name">
+ <label for="email">Email:</label>
+ <input type="text" name="email">
+
+ <label for="major">Select major</label>
+ <input type="radio" name="major" value="computerScience">Computer Science <br>
+ <input type="radio" name="major" value="wdd">WDD <br>
+ <input type="radio" name="major" value="cit">Computer Information Technology<br>
+ <input type="radio" name="major" value="computerEngineering">Computer Engineering <br>
+
+ <label for="name">Comments:</label>
+ <textarea name="comments" id="comments" cols="30" rows="10"></textarea>
+</form>
+</body>
