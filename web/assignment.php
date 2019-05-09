d7549b168370dcda375490a1833d5e2c0b91347e
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-09 15:28:32 -0600

    week 3

--- web/assignment.php
+++ web/assignment.php
@@ -20,8 +20,8 @@ include("nav.php");
     <main>
         <p>Assignments are like poop. They stink and you usually don't notice them coming until you step in it.</p>
         <ul>
-            <li class="a-item"><a href="/week2/home.php">Week 2</a></li>
-            <li class="a-item"><a href="/week3/index.php">Week 3</a></li>
+            <li class="a-item"><a href="/week2/home.php">Week 2</a></li><br>
+            <li class="a-item"><a href="/week3/index.php">Week 3</a></li><br>
         </ul>
     </main>
 </body>
