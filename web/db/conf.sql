5fed62e871168e006c3c43a219ba3fde3b08aebe
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-16 16:37:26 -0600

    db

---
+++ web/db/conf.sql
@@ -0,0 +1,40 @@
+CREATE TABLE confUser (
+	user_id		int NOT NULL PRIMARY KEY,
+	username	varchar(80) NOT NULL unique,
+	password	varchar(80) NOT NULL
+);
+
+CREATE TABLE talk (
+	talk_id		int NOT NULL PRIMARY KEY,
+	title		varchar(80) NOT NULL,
+	speaker		varchar(80) NOT NULL
+);
+
+CREATE TABLE conf (
+	conf_id		int NOT NULL PRIMARY KEY,
+	year		varchar(80) NOT NULL,
+	month		varchar(80) NOT NULL
+);
+
+CREATE TABLE notes (
+	note_id		int NOT NULL PRIMARY KEY,
+	title		varchar(80) NOT NULL,
+	text		text NOT NULL,
+	user_id		int NOT NULL REFERENCES confUser(user_id),
+	talk_id		int NOT NULL REFERENCES talk(talk_id),
+	conf_id		int NOT NULL REFERENCES conf(conf_id)
+);
+
+INSERT INTO confuser VALUES (1, 'username', 'password123');
+INSERT INTO talk VALUES (1, 'HOPE', 'Bro. Thayne');
+INSERT INTO conf VALUES (1, '2019', 'April');
+
+INSERT INTO notes VALUES (1, 'Hope', 'I like this!', 1, 1, 1);
+INSERT INTO notes VALUES (2, 'Another Talk', 'Cool!');
+
+DROP TABLE confUser;
+DROP TABLE notes;
+DROP TABLE talk;
+DROP TABLE conf;
+
+SELECT * FROM notes;
---
+++ web/shopping-cart/shared/layout_header.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+ 
+    <title><?php echo isset($page_title) ? $page_title : "Shopping Cart"; ?></title>
+ 
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"> 
+    <link href="../styles.css" rel="stylesheet" media="screen">
+ 
+</head>
+<body>
+ 
+    <?php include 'navigation.php'; ?>
+
+    <div class="container">
+        <div class="row">
+ 
+        <div class="col-md-12">
+            <div class="page-header">
+                <h1><?php echo isset($page_title) ? $page_title : "Shopping Cart"; ?></h1>
+            </div>
+        </div>
