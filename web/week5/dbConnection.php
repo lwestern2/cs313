55aa1ff26aa70a03cb7f5e544323c5a4f5928a53
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-25 21:30:53 -0600

    week 5

--- web/week5/dbConnection.php
+++ web/week5/dbConnection.php
@@ -3,7 +3,7 @@ function getDb() {
     $db = NULL;
 
     try {
-        $dbUrl = pg_connect(getenv("DATABASR_URL"));
+        $dbUrl = pg_connect(getenv("DATABASE_URL"));
 
         $dbOpts = parse_url($dbUrl);
         // $dbUser = 'ta_user';
@@ -12,15 +12,13 @@ function getDb() {
         // $dbHost = 'localhost';
         // $dbPort = '5432';
 
-        // $dbHost = $dbOpts["host"];
-        // $dbPort = $dbOpts["port"];
-        // $dbUser = $dbOpts["user"];
-        // $dbPassword = $dbOpts["pass"];
-        // $dbName = ltrim($dbOpts["path"], '/');
+        $dbHost = $dbOpts["host"];
+        $dbPort = $dbOpts["port"];
+        $dbUser = $dbOpts["user"];
+        $dbPassword = $dbOpts["pass"];
+        $dbName = ltrim($dbOpts["path"], '/');
 
-        $db = new PDO("pgsql:host=ec2-54-235-167-210.compute-1.amazonaws.com
-        ;dbname=d6oki00667h33u", 'smorcmcpfbcunb', 'True2god'
-        ');
+        $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
