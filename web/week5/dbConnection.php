eef26b145b57a17d4819f16107b9caf16a1d0138
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-25 21:35:45 -0600

    week 5

--- web/week5/dbConnection.php
+++ web/week5/dbConnection.php
@@ -3,24 +3,19 @@ function getDb() {
     $db = NULL;
 
     try {
-        $dbUrl = pg_connect(getenv("DATABASE_URL"));
+        $dbUrl = getenv('DATABASE_URL');
 
-        $dbOpts = parse_url($dbUrl);
-        // $dbUser = 'ta_user';
-        // $dbPassword = 'ta_pass';
-        // $dbName = 'scripture_ta';
-        // $dbHost = 'localhost';
-        // $dbPort = '5432';
+  $dbOpts = parse_url($dbUrl);
 
-        $dbHost = $dbOpts["host"];
-        $dbPort = $dbOpts["port"];
-        $dbUser = $dbOpts["user"];
-        $dbPassword = $dbOpts["pass"];
-        $dbName = ltrim($dbOpts["path"], '/');
+  $dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
 
-        $db = new PDO("pgsql:host=$dbHost;dbname=$dbName", $dbUser, $dbPassword);
+  $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
-        $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
     catch (PDOException $ex) {
