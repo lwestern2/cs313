bc7091257c4a9b4c9b66cdee5115432dc80a9ada
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-25 21:22:51 -0600

    week 5

--- web/week5/dbConnection.php
+++ web/week5/dbConnection.php
@@ -18,7 +18,8 @@ function getDb() {
         // $dbPassword = $dbOpts["pass"];
         // $dbName = ltrim($dbOpts["path"], '/');
 
-        $db = new PDO("pgsql:host=localhost;dbname=d6oki00667h33u", 'smorcmcpfbcunb', '6613fbb213fe196a1f1c7d70b24d9a800b185f8996da01ac94bf19ffd74bcfe3
+        $db = new PDO("pgsql:host=ec2-54-235-167-210.compute-1.amazonaws.com
+        ;dbname=d6oki00667h33u", 'smorcmcpfbcunb', '6613fbb213fe196a1f1c7d70b24d9a800b185f8996da01ac94bf19ffd74bcfe3
         ');
 
         $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
