eaa1309f8c500165d87adeb792244ad654d34aae
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-29 23:58:31 -0600

    week5

--- web/week5/scriptureView.php
+++ web/week5/scriptureView.php
@@ -24,7 +24,7 @@ $db = getDb();
             $verse = $row['verse'];
             $content = $row['content'];
 
-            echo "<p><strong>$book $chapter:$verse</strong> - \"$content\"<p>";
+            echo "<a href="scriptureDetails.php"><strong>$book $chapter:$verse</strong><a>";
         }
 
         ?>
