a0226f39d0e9dd1c47c8eb65ca20239b799cdc9c
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-30 16:29:16 -0600

    week6

--- web/week5/scriptureView.php
+++ web/week5/scriptureView.php
@@ -19,13 +19,14 @@ $db = getDb();
         $statement->execute();
 
         while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $id = $row['id'];
             $book = $row['book'];
             $chapter = $row['chapter'];
             $verse = $row['verse'];
             $content = $row['content'];
 
-            echo "<p><strong>$book $chapter:$verse</strong> - <p>";
-            echo "<a href='scriptureDetails.php'>View More</a>";
+            echo "<p><strong>$book $chapter:$verse</strong><p>";
+            echo "<a href='scriptureDetails.php?id='" . $row['id'] . "'>View More</a>";
         }
 
         ?>
