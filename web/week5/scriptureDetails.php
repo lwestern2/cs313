eb0b74ab680234464a9b3d26293ab4df879cabf4
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-30 16:20:50 -0600

    week6

--- web/week5/scriptureDetails.php
+++ web/week5/scriptureDetails.php
@@ -15,18 +15,19 @@
         <h1>Scripture Details</h1>
 
 <?php
-    $statement = $db->query('SELECT book, chapter, verse, content FROM scripture');
+    $statement = $db->query('SELECT id, book, chapter, verse, content FROM scripture');
     $statement->execute();
 
-        $book = $row['book'];
-        $chapter = $row['chapter'];
-        $verse = $row['verse'];
-        $content = $row['content'];
+    //     $book = $row['book'];
+    //     $chapter = $row['chapter'];
+    //     $verse = $row['verse'];
+    //     $content = $row['content'];
 
-    $stmt = $db->prepare('SELECT * FROM scripture WHERE book=:book AND chapter=:chapter AND verse=:verse ');
-    $stmt->bindValue(':book', $book, PDO::PARAM_STR);
-    $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
-    $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
+    $stmt = $db->prepare('SELECT * FROM scripture WHERE id = :id');
+    $stmtTopics->bindValue(':id', $row['id']);
+    // $stmt->bindValue(':book', $book, PDO::PARAM_STR);
+    // $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
+    // $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
     $stmt->execute();
 
     $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
@@ -37,6 +38,8 @@
         echo '<p>'.$row['content'].'</p>';
     }
 ?>
+
+<a href="scriptureView.php">View All</a>
 </div>
 </body>
 </html>
