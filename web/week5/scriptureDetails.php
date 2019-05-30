82d12d834820bfb721d1d0d2b3206865d3be3bc0
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-30 16:10:17 -0600

    week6

--- web/week5/scriptureDetails.php
+++ web/week5/scriptureDetails.php
@@ -2,14 +2,16 @@
     include "dbConnection.php";
     $db = getDb();
 
-    $queries = array();
-    parse_str($_SERVER['QUERY_STRING'], $queries);
-    
-    $book = $queries['book'];
-    $chapter = $queries['chapter'];
-    $verse = $queries['verse'];
+    $statement = $db->query('SELECT book, chapter, verse, content FROM scripture');
+    $statement->execute();
 
-    $stmt = $db->prepare('SELECT * FROM to_scriptures WHERE book=:book AND chapter=:chapter AND verse=:verse ');
+    while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+        $book = $row['book'];
+        $chapter = $row['chapter'];
+        $verse = $row['verse'];
+        $content = $row['content'];
+
+    $stmt = $db->prepare('SELECT * FROM scripture WHERE book=:book AND chapter=:chapter AND verse=:verse ');
     $stmt->bindValue(':book', $book, PDO::PARAM_STR);
     $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
     $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
