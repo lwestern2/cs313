9a1e1b7a6656017e553bf06bb8129b1ea808bf9c
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-29 23:56:57 -0600

    week5

--- db/myDb.sql
+++ db/myDb.sql
@@ -1,32 +1,31 @@
 -- a3ab69363b4c84cc276e0c3dcc8a4172e66b50ee9c3c5afff530f46796375789
 
 CREATE TABLE hw_user (
-	user_id		int NOT NULL PRIMARY KEY,
+	user_id		SERIAL NOT NULL PRIMARY KEY,
 	username	varchar(80) NOT NULL unique,
 	password	varchar(80) NOT NULL
 );
 
 CREATE TABLE hw (
-	hw_id		int NOT NULL PRIMARY KEY,
+	hw_id		SERIAL NOT NULL PRIMARY KEY,
     data        Date NOT NULL,
 	hw_name		varchar(80) NOT NULL,
 	class_code	varchar(80) NOT NULL,
     due_date    Date NOT NULL
-
 );
 
 CREATE TABLE to_do (
-	list_id		int NOT NULL PRIMARY KEY,
+	list_id		SERIAL NOT NULL PRIMARY KEY,
 	list_text	varchar(80) NOT NULL,
 	due_date	Date NOT NULL,
     data        Date NOT NULL
 );
 
 CREATE TABLE calendar (
-	cal_id		int NOT NULL PRIMARY KEY,
-	hw_id		int NOT NULL REFERENCES hw(hw_id),
-	list_id		int NOT NULL REFERENCES to_do(list_id),
-	user_id		int NOT NULL REFERENCES hw_user(user_id)
+	cal_id		SERIAL NOT NULL PRIMARY KEY,
+	hw_id		SERIAL NOT NULL REFERENCES hw(hw_id),
+	list_id		SERIAL NOT NULL REFERENCES to_do(list_id),
+	user_id		SERIAL NOT NULL REFERENCES hw_user(user_id)
 );
 
 INSERT INTO hw_user VALUES (1, 'username', 'password123');
