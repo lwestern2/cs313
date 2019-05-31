b7307dac697cca0bdcec5b02e0a9d3b4f66fe064
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-05-31 11:21:09 -0600

    week5

--- web/week5/scriptureDetails.php
+++ web/week5/scriptureDetails.php
@@ -17,17 +17,17 @@
 <?php
 
 
-    $statement = $db->query('SELECT id, book, chapter, verse, content FROM scripture');
-    $statement->execute();
+    $statement = $db->query('SELECT id, book, chapter, verse, content FROM scripture WHERE id =' . $_GET['id']);
+    // $statement->execute();
 
-    $stmt = $db->prepare('SELECT * FROM scripture WHERE id = :id');
-    $stmtTopics->bindValue(':id', $row['id']);
+    // $stmt = $db->prepare('SELECT * FROM scripture WHERE id = :id');
+    // $stmtTopics->bindValue(':id', $row['id']);
     // $stmt->bindValue(':book', $book, PDO::PARAM_STR);
     // $stmt->bindValue(':chapter', $chapter, PDO::PARAM_INT);
     // $stmt->bindValue(':verse', $verse, PDO::PARAM_INT);
-    $stmt->execute();
+    // $stmt->execute();
 
-    $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $rows = $statement->fetch(PDO::FETCH_ASSOC);
 
     foreach ($rows as $row){
         echo '<strong>' . $row['book'] .' '. $row['chapter'] .':'. $row['verse'];
