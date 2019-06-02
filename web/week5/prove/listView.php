aa7a4a5bed3aae09d030a45164056dad469b92be
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-02 01:23:59 -0600

    week5

--- web/week5/prove/listView.php
+++ web/week5/prove/listView.php
@@ -27,12 +27,36 @@ $db = getDb();
             $class = $row['class_code'];
             $due = $row['due_date'];
 
-            echo "<p><strong>$class: $name - $due</strong><p>";
+            echo "<p><strong>$class: $name</strong></p>"
+            echo "<p>Due: $due<p>";
             echo '<a href="details.php?id=' . $row['hw_id'];
             echo '">View More</a>';
         }
 
         ?>
+
+        <div>
+        <h2>To do</h2>
+
+        <?php
+        $statement = $db->query('SELECT list_id, list, list_text, date_done, date_add FROM to_do');
+        $statement->execute();
+
+        while ($row = $statement->fetch(PDO::FETCH_ASSOC)) {
+            $id = $row['list_id'];
+            $date = $row['date_add'];
+            $list = $row['list'];
+            $text = $row['list_text'];
+            $done = $row['date_done'];
+
+            echo "<p><strong>$list</strong></p>"
+            echo "<p>Do by: $done<p>";
+            echo '<a href="details.php?id=' . $row['list_id'];
+            echo '">View More</a>';
+        }
+
+        ?>
+        </div>
     </div>
 </body>
 
