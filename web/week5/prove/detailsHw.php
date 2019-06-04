71c964f7ef7036711113821b9b35147694b966ed
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-03 21:38:58 -0600

    week 5

--- web/week5/prove/detailsHw.php
+++ web/week5/prove/detailsHw.php
@@ -14,17 +14,13 @@ $db = getDb();
 
 <?php
 
-    $stmt = $db->prepare('SELECT hw_id, date_add, hw_name, hw_text, class_code, due_date FROM hw WHERE hw_id = :id');
-    // $statement->execute();
-
-    // $stmt = $db->prepare('SELECT * FROM scripture WHERE id = :id');
-    // $stmtTopics->bindValue(':id', $row['id']);
-    $stmt->bindValue(':id', $_GET['hw_id'], PDO::PARAM_INT);
+    $stmt = $db->prepare('SELECT hw_id, date_add, hw_name, hw_text, class_code, due_date FROM hw WHERE hw_id = :hw_id');
+    $stmt->bindValue(':hw_id', $_GET['hw_id'], PDO::PARAM_INT);
     $stmt->execute();
 
     $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        echo '<strong>' . $row['class_code'] . ': ' . $row['hw_name'] . '</strong>';
+        echo '<p><strong>' . $row['class_code'] .': '. $row['hw_name'] . '</strong></p>';
         echo '<p>' . $row['hw_text'] . '</p>';
         echo '<p style="color: red;"><strong>Due: '. $row['due_date'] . '</strong></p>';
         echo '<p>Date Added: ' . $row['date_add'] . '</p>';
