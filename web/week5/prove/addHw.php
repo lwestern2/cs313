95f10bd68eb9a1e9715db98df60f4c85ad3f70d1
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-03 22:17:17 -0600

    week 5

--- web/week5/prove/addHw.php
+++ web/week5/prove/addHw.php
@@ -13,35 +13,34 @@ $db = getDb();
 <div>
 <h1>Enter A New Homework Assignment</h1>
 
-<form id="mainForm" action="hwInsert.php" method="POST">
-    <label for="class">Class</label>
-	<input type="text" id="class_code" name="class">
-	<br /><br />
-
-	<label for="name">Assignment Title</label>
-	<input type="text" id="hw_name" name="name">
-	<br /><br />
-
-	<label for="hw_text">Instructions</label>
-	<input type="text" id="hw_text" name="hw_text">
-	<br /><br />
-
-	<label for="due">Due Date</label>
-	<input type="date" id="due_date" name="due">
-	<br /><br />
-
-	<label for="date">Date Added:</label><br />
-    <input type="date" value="<?php echo $today; ?>" id="date" name="date">
-	<br /><br />
-
-    <?php 
+<?php 
     $month = date('m');
     $day = date('d');
     $year = date('Y');
 
     $today = $year . '-' . $month . '-' . $day;
     ?>
-	<br>
+
+<form id="mainForm" action="hwInsert.php" method="POST">
+    <label for="class">Class:</label>
+	<input type="text" id="class_code" name="class">
+	<br /><br />
+
+	<label for="name">Assignment Title:</label>
+	<input type="text" id="hw_name" name="name">
+	<br /><br />
+
+	<label for="hw_text">Instructions:</label>
+	<textarea id="hw_text" name="hw_text" rows="4" cols="50"></textarea>
+	<br /><br />
+
+	<label for="due">Due Date:</label>
+	<input type="date" id="due_date" name="due">
+	<br /><br />
+
+	<label for="date">Date Added:</label>
+    <input type="date" value="<?php echo $today; ?>" id="date" name="date">
+	<br /><br />
 
 	<input type="submit" value="Add Homework Assignment">
 </form>
