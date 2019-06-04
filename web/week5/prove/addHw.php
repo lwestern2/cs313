64b9dbe86641925be1861efa0425ad61a581c704
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-03 22:29:48 -0600

    week 5

--- web/week5/prove/addHw.php
+++ web/week5/prove/addHw.php
@@ -22,24 +22,24 @@ $db = getDb();
     ?>
 
 <form id="mainForm" action="hwInsert.php" method="POST">
-    <label for="class">Class:</label>
-	<input type="text" id="class_code" name="class">
+    <label for="class_code">Class:</label>
+	<input type="text" id="class_code" name="class_code">
 	<br /><br />
 
-	<label for="name">Assignment Title:</label>
-	<input type="text" id="hw_name" name="name">
+	<label for="hw_name">Assignment Title:</label>
+	<input type="text" id="hw_name" name="hw_name">
 	<br /><br />
 
 	<label for="hw_text">Instructions:</label><br>
 	<textarea id="hw_text" name="hw_text" rows="4" cols="50"></textarea>
 	<br /><br />
 
-	<label for="due">Due Date:</label>
-	<input type="date" id="due_date" name="due">
+	<label for="due_date">Due Date:</label>
+	<input type="date" id="due_date" name="due_date">
 	<br /><br />
 
-	<label for="date">Date Added:</label>
-    <input type="date" value="<?php echo $today; ?>" id="date" name="date">
+	<label for="date_add">Date Added:</label>
+    <input type="date" value="<?php echo $today; ?>" id="date_add" name="date_add">
 	<br /><br />
 
 	<input type="submit" value="Add Homework Assignment">
