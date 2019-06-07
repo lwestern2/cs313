b202f62ea7f5dd41d63125c7d92c63ff9d1c715a
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-07 01:18:12 -0600

    week 6

---
+++ web/week6/prove/hwUpdate.php
@@ -0,0 +1,21 @@
+<?php
+require("dbConnection.php");
+$db = getDb();
+
+$hwid = $_POST['hw_id'];
+$hwdate = $_POST['date_add'];
+$hwname = $_POST['hw_name'];
+$hwtext = $_POST['hw_text'];
+$class = $_POST['class_code'];
+$due = $_POST['due_date'];
+
+$stmt = $db->prepare('UPDATE hw SET class_code = $class,
+hw_name = $hwname, hw_text = $hwtext,
+due_date = $due, date_add = $hwdate WHERE hw_id = $hwid');
+
+$stmt->execute();
+
+if($stmt) {
+	echo "Done";
+}
+?>
