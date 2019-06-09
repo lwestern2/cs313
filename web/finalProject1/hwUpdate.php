329821ddc64983b16c87e2702a24bfd6149d9572
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-09 00:35:22 -0600

    project

--- web/finalProject1/hwUpdate.php
+++ web/finalProject1/hwUpdate.php
@@ -2,19 +2,19 @@
 require("dbConnection.php");
 $db = getDb();
 
-$hwid = $_POST['hw_id'];
+$hwid = $_GET['hw_id'];
 $hwdate = $_POST['date_add'];
 $hwname = $_POST['hw_name'];
 $hwtext = $_POST['hw_text'];
 $class = $_POST['class_code'];
 $due = $_POST['due_date'];
 
-echo "ID: " . $_GET['hw_id'];
+echo "ID: $hwid";
 
 try {
 $query = ("UPDATE hw SET class_code = '$class',
 hw_name = '$hwname', hw_text = '$hwtext',
-due_date = '$due', date_add = '$hwdate' WHERE hw_id = $hwid");
+due_date = '$due', date_add = '$hwdate' WHERE hw_id = $_GET[hw_id]");
 $stmt = $db->prepare($query);
 $stmt->execute();
 }
