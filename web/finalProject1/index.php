1a60ad454a1c14702cdf32efb5b7d466f7fbbb77
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-10 20:43:35 -0600

    project

--- web/finalProject1/index.php
+++ web/finalProject1/index.php
@@ -9,9 +9,43 @@ include 'functions.php';
     <title>Homework Calendar</title>
     <meta charset="utf-8">
 
-    <link rel="stylesheet" href="style.css">
-    <div id="calendar"></div>
-    <script src="functions.js"></script>
-    <script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.5.1/moment.min.js"></script>
+<style>
+.yui3-button {
+    margin:10px 0px 10px 0px;
+    color: #fff;
+    background-color: #3476b7;
+}
+</style>
+
+<div id="demo" class="yui3-skin-sam yui3-g"> <!-- You need this skin class -->
+
+  <div id="leftcolumn" class="yui3-u">
+     <div id="mycalendar"></div>
+  </div>
+  <div id="rightcolumn" class="yui3-u">
+   <div id="links" style="padding-left:20px;">
+      Events on: <span id="selecteddate"></span>
+   </div>
+  </div>
+</div>
+
+<script type="text/javascript">
+YUI().use('calendar', 'datatype-date', 'cssbutton',  function(Y) {
+    var calendar = new Y.Calendar({
+      contentBox: "#mycalendar",
+      width:'340px',
+      showPrevMonth: true,
+      showNextMonth: true,
+      date: new Date()}).render();
+
+    var dtdate = Y.DataType.Date;
+
+    calendar.on("selectionChange", function (ev) {
+      var newDate = ev.newSelection[0];
+
+      Y.one("#selecteddate").setHTML(dtdate.format(newDate));
+    });
+});
+</script>
 
 </html>
