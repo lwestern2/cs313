336c118cb2e9c3c4e3c6c16444d8693813f8727d
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-10 23:37:26 -0600

    project

--- web/finalProject1/detailsList.php
+++ web/finalProject1/detailsList.php
@@ -8,31 +8,35 @@ $db = getDb();
 
 <head>
     <title>To Do List Details</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 
-<h3>To-Do List Details</h3>
+<?php
+$stmt = $db->prepare('SELECT list_id, list, list_text, date_done, date_add FROM to_do WHERE list_id = :list_id');
+$stmt->bindValue(':list_id', $_GET['list_id'], PDO::PARAM_INT);
+$stmt->execute();
+
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
+<h3>List Details for <?php echo $row['list']; ?></h3>
 
 <?php
-
-    $stmt = $db->prepare('SELECT list_id, list, list_text, date_done, date_add FROM to_do WHERE list_id = :list_id');
-    $stmt->bindValue(':list_id', $_GET['list_id'], PDO::PARAM_INT);
-    $stmt->execute();
-
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         echo '<p><strong>' . $row['list'] .'</strong></p>';
         echo '<p>' . $row['list_text'] . '</p>';
         echo '<p style="color: red;"><strong>Do by: '. $row['date_done'] . '</strong></p>';
         echo '<p>Date Added: ' . $row['date_add'] . '</p>';
 
-        echo '<a href="editList.php?list_id=' . $row['list_id'];
+        echo '<a class="btn" href="editList.php?list_id=' . $row['list_id'];
         echo '">Edit</a>';
 
-        echo '<a href="deleteList.php?list_id=' . $row['list_id'];
+        echo '<a class="btn-danger" href="deleteList.php?list_id=' . $row['list_id'];
         echo '">Delete</a>';
 ?>
 <br>
-<a href="listView.php">View All</a>
+<br>
+<a class="btn" href="listView.php">View All</a>
 
 </body>
 
