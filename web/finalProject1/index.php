76aed64ab1e0ec0bb6d4596fb5b2e7cd73efcdc8
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-10 21:18:01 -0600

    project

--- web/finalProject1/index.php
+++ web/finalProject1/index.php
@@ -12,19 +12,7 @@
     <script src="functions.js"></script>
 </head>
 
-<div class="container" id="main">
-    <div class="jumbotron">
-        <h1 class="text-center">
-            <a id="left" href="#"><i class="fa fa-chevron-left"> </i></a>
-            <span>&nbsp;</span><span id="month"> </span><span>&nbsp;</span>
-            <span id="year"> </span><span>&nbsp;</span><a id="right" href="#"><i class="fa fa-chevron-right"> </i></a>
-        </h1>
-    </div>
-    <div class="row">
-        <div class="col-sm-10 col-sm-offset-1">
-            <table class="table"></table>
-        </div>
-    </div>
+<div id="calendar" class="calendar">
 </div>
 
 </html>
