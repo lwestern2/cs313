34a5fba7f1e80859a083d5139f2a070c1d014d05
lwestern2 <35503015+lwestern2@users.noreply.github.com>
2019-06-10 23:28:55 -0600

    project

--- web/finalProject1/detailsHw.php
+++ web/finalProject1/detailsHw.php
@@ -11,15 +11,17 @@ $db = getDb();
     <link rel="stylesheet" href="style.css">
 </head>
 
+<?php
+$stmt = $db->prepare('SELECT hw_id, date_add, hw_name, hw_text, class_code, due_date FROM hw WHERE hw_id=:hw_id');
+$stmt->bindValue(':hw_id', $_GET['hw_id'], PDO::PARAM_INT);
+$stmt->execute();
+
+$row = $stmt->fetch(PDO::FETCH_ASSOC);
+?>
+
 <h3 class="heading">Homework Details for <?php echo $row['hw_name']; ?></h3>
 
 <?php
-
-    $stmt = $db->prepare('SELECT hw_id, date_add, hw_name, hw_text, class_code, due_date FROM hw WHERE hw_id=:hw_id');
-    $stmt->bindValue(':hw_id', $_GET['hw_id'], PDO::PARAM_INT);
-    $stmt->execute();
-
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         echo '<p><strong>' . $row['class_code'] .': '. $row['hw_name'] . '</strong></p>';
         echo '<p>' . $row['hw_text'] . '</p>';
